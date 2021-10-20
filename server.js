09dd88b2f7f6a646f03f8d1b896ad24859d0aa07
Alex Gibson <argibson02@gmail.com>
2021-10-19 20:31:44 -0700

    Wrote basic app.get and app.listen instructions on server.js file

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const PORT = 3001;
-// const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+);
