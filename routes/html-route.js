ffabe34f3017cfbabddb563078b5843996c26653
Alex Gibson <argibson02@gmail.com>
2021-10-19 22:10:22 -0700

    Added router code to html-route.js. Required and added in the route in server.js. Added notes in both

--- routes/html-route.js
+++ routes/html-route.js
@@ -1,2 +1,13 @@
+// General Express declarations and requires
 const express = require('express');
-const path = require('path');
+const routerApp = require("express").Router();
+
+// Loop in the notes API by requiring it
+const notesApiRouter = require("./api-route");
+
+// Set the URL route to look for /notes to access the notes API
+routerApp.use("/notes", notesApiRouter);
+
+// Export this
+module.exports = routerApp;
+
--- server.js
+++ server.js
@@ -1,22 +1,35 @@
+// General Express declarations and requires
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const routerApp = require('./routes/html-route');
+
+// Our PORt is either 3000 for local or the host (Heroku) PORT
 const PORT = process.env.PORT || 3000;
 
+// Invoke express
 const app = express();
 
+// JSONify and use the body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Gives express access to the files in the "public" folder
 app.use(express.static('public'));
 
+// If /notes is used, we defer to the router in html-route for further instructions
+app.use("/notes", routerApp);
+
+// Get for landing page
 app.get("/", (req, res) =>
     res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
-app.get("/notes", (req, res) =>
-    res.sendFile(path.join(__dirname, "/public/notes.html"))
-);
+// app.get("/notes", (req, res) =>
+//     res.sendFile(path.join(__dirname, "/public/notes.html"))
+// );
 
+// Listener
 app.listen(PORT, () =>
 console.log(`Note is listening and available in at http://localhost${PORT}`)
 );
