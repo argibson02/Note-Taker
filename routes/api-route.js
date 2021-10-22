8a419e987a87b3e87b36f642751219f68c7357a5
Alex Gibson <argibson02@gmail.com>
2021-10-21 17:31:37 -0700

    Added annotations to the api-route file.

--- routes/api-route.js
+++ routes/api-route.js
@@ -29,43 +29,44 @@ notes.get("/api/notes", (req, res) => {
 
 // GET note by unique ID saved on db.json
 notes.get("/api/notes/:id", (req, res) => {
-    // promisifies it so that action is only taken when all the body has been received 
+    // promisifies it so that action is only taken when all the body has been received.
     readFromFile("./db/db.json").then((data) => {
-        let parsed = JSON.parse(data)
-        res.json(parsed[req.params.id])
+        let parsed = JSON.parse(data) // parses data to JSON object.
+        res.json(parsed[req.params.id]) // returns only the object with uuid that you had just selected.
     })
 });
 
 
 // POST notes and given them unique IDs
 notes.post('/api/notes', (req, res) => {
-    req.body.id = uuidv4();
 
-    function readAndAppend(content, file) {
+    req.body.id = uuidv4();  // Adds the uuid to each object in the note array here.
+
+    function readAppendRewrite(content, file) {
         fs.readFile(file, 'utf8', (err, data) => {
             if (err) {
-                console.error(err);
+                console.error(err); // Error checking
             }
             else {
-                let parsedData = JSON.parse(data);
-                parsedData.push(content);
-                writeToFile(file, parsedData);
+                let parsed = JSON.parse(data); // parses data to JSON object.
+                parsed.push(content); // pushes the new req.body to the JSON object.
+                writeToFile(file, parsed);  // Rewrites the database file with the new object.
             }
         })
     }
-    readAndAppend(req.body, "./db/db.json");
+    readAppendRewrite(req.body, "./db/db.json"); // invokes function with passed values
 
     let notePost = {
         body: req.body
     }
-    res.json(notePost);
+    res.json(notePost); // hmmmm, could put inside readAppendRewrite???
 });
 
 
-// DELETE note by by filtering out unique ID and rewriting file.
+// DELETE note by by filtering out unique ID and rewriting file.               // how is delete different from post? I am technically using this wrong?
 notes.delete("/api/notes/:id", (req, res) => {
-    // console.log(req.params.id);
-    let currentId = req.params.id;
+
+    let currentId = req.params.id; // Preps for filtering out this ID
 
     function readRemoveRewrite(content, file) {
         fs.readFile(file, 'utf8', (err, data) => {
@@ -73,14 +74,14 @@ notes.delete("/api/notes/:id", (req, res) => {
                 console.error(err);
             }
             else {
-                let parsed = JSON.parse(data);
-                let sansCurrentId = parsed.filter(note => note.id !== currentId);
-                writeToFile(file, sansCurrentId);
+                let parsed = JSON.parse(data); // parses data to JSON object.
+                let sansCurrentId = parsed.filter(note => note.id !== currentId); // Now that it is in a JSON obj, we filter out the currentId.
+                writeToFile(file, sansCurrentId); // Rewrites the database file with the new object.
                 res.json(sansCurrentId);
             }
         })
     }
-    readRemoveRewrite(req.body, "./db/db.json");
+    readRemoveRewrite(req.body, "./db/db.json"); // invokes function with passed values
 
 });
 
